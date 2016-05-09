6f408871404d20fb130dc8d36b896f78f0ab6788
Javier Fong <Javier Fong@192.168.1.6>
2016-05-09 11:18:30 -0600

    GUI

---
+++ src/dbproject2/DBProject2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbproject2;
+
+import GUI.mainView;
+
+/**
+ *
+ * @author Javier Fong
+ */
+public class DBProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mainView mv = new mainView();
+        
+        mv.setTitle("Clientes");
+        mv.setVisible(true);
+    }
+    
+}
